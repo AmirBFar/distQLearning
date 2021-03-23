5842950eba298a1a3494674bd146f551d70e3022
Amir Behrouzi-Far <40320427+AmirBFar@users.noreply.github.com>
2021-03-23 10:19:50 -0400

    Update sarsa_multithread.py

--- sarsa_multithread.py
+++ sarsa_multithread.py
@@ -48,59 +48,55 @@ class Sarsa():
         learner_thread.join()
 
     def learner(self):
+        
         update_cnt = 0
         policy = epsilon_greedy(self.Q,self.env.action_space.n)
+        
         while self.flag:
-            #print("I am learner")
+            
             if self.temp_buffer:
-                #print(len(self.temp_buffer))
+                
                 if self.learner_policy == "LCFS":
                     exp = self.temp_buffer.pop(-1)
                 else:
                     exp = self.temp_buffer.pop(0)
-                #exp = self.temp_buffer.pop(-1)
-                #next_greedy_action = self.Q[exp[3]].index(max(self.Q[exp[3]]))                                                                                                                    
-                #action_probs = policy(exp[3],self.eps)
-                #next_action = np.random.choice(np.arange(
-                #    len(action_probs)),
-                #   p = action_probs)
+                
                 next_action = exp[4]
                 td_target = exp[2] + self.gamma*self.Q[exp[3]][next_action]                                                                                                                           
                 td_delta = td_target - self.Q[exp[0]][exp[1]]                                                                                                                                                    
                 self.Q[exp[0]][exp[1]] = (1-self.alpha)*self.Q[exp[0]][exp[1]] + self.alpha*td_target  
+                
                 update_cnt += 1
-            #time.sleep(.2)
-        #print('The total number of updates is ',update_cnt)
+           
+        print('The total number of updates is ',update_cnt)
         
 
     def qLearning(self):
+        
         policy = epsilon_greedy(self.Q,self.env.action_space.n)
         print("I am actor")
         step_cnt = 0
+        
         if self.learner_policy not in ["FCFS","LCFS"]:
             window = int(self.learner_policy.split(' ')[3])
+            
         for i in range(self.num_episodes):
-            #if i%10 == 0:
-                #print('episode %d started'%i)
-                #print('buffer length is',len(self.temp_buffer))
-            #state = tuple(self.env.reset()) ### for the environments with mutable state representation
+           
             state = self.env.reset()
             action_probs = policy(state,self.eps)
             action = np.random.choice(np.arange(
                     len(action_probs)),
                     p = action_probs)
             for t in itertools.count():
-                #action_probs = policy(state,self.eps)
-                #action = np.random.choice(np.arange(
-                #    len(action_probs)),
-                #    p = action_probs)
+                
                 next_state, reward, done, _ = self.env.step(action)
-                #next_state = tuple(next_state) ### for the enviroments with mutable state representation
                 step_cnt += 1
+                
                 action_probs = policy(next_state,self.eps)
                 next_action = np.random.choice(np.arange(
                     len(action_probs)),
                     p = action_probs)
+                
                 if self.learner_policy in ["FCFS","LCFS"]:
                     self.temp_buffer.append([state,action,reward,next_state,next_action])
                 else:
@@ -116,12 +112,12 @@ class Sarsa():
                     break
                 state = next_state
                 action = next_action
+                
         self.flag = False
-        #print('The total number of steps is ',step_cnt)
+        print('The total number of steps is ',step_cnt)
 
 if __name__ == "__main__":
-    #env = gym.make('WindyGridworld-v0')
-    #env = gym.make('CartPole-v0')
+
     env = 'WindyGridworld-v0'
     Q = Qlearning(env,1,5000,.99,.1)
     print(Q.stats.episode_rewards[::10])
